929a3f37461c09e0e54375df0931780aede0260d
nickbreaton <nick@breaton.com>
2018-03-29 13:11:50 -0400

    Add google sign in

--- client/main.js
+++ client/main.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Tracker } from 'meteor/tracker'
+
+Tracker.autorun(() => {
+  console.log(Meteor.user())
+})
+
+const login = () => {
+  Meteor.loginWithGoogle({
+    requestPermissions: [
+      'https://www.googleapis.com/auth/calendar.readonly'
+    ]
+  }, (err) => {
+    if (err) {
+      // handle error
+    } else {
+      // successful login!
+    }
+  });
+}
 
 const App = () => (
-  <h1>Hello world</h1>
+  <button onClick={login}>Hello world</button>
 );
 
 Meteor.startup(() => {
--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // ServiceConfiguration.configurations.insert({
+  //   service: 'google',
+  //   clientId: '1026154799197-2fht9b4u943775lmaokpi8ijk8boio19.apps.googleusercontent.com',
+  //   loginStyle: 'popup',
+  //   secret: 'SDrISTVcyKLDQiZmHzyRae4C'
+  // })
 });
