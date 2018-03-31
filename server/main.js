b0feddcdebea1e62c67baa1c705051cab482187a
nickbreaton <nick@breaton.com>
2018-03-31 09:58:12 -0400

    Fix something I broke

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor'
 
 Meteor.startup(() => {
-    ServiceConfiguration.configurations.insert({
-        service: 'google',
-        clientId: '1026154799197-2fht9b4u943775lmaokpi8ijk8boio19.apps.googleusercontent.com',
-        loginStyle: 'popup',
-        secret: 'SDrISTVcyKLDQiZmHzyRae4C'
-    })
+    // ServiceConfiguration.configurations.insert({
+    //     service: 'google',
+    //     clientId: '1026154799197-2fht9b4u943775lmaokpi8ijk8boio19.apps.googleusercontent.com',
+    //     loginStyle: 'popup',
+    //     secret: 'SDrISTVcyKLDQiZmHzyRae4C'
+    // })
 })
