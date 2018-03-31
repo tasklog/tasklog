28fff74941f384731147689ebac999cca999ff0a
ethomp54 <ethomp54@uncc.edu>
2018-03-31 09:47:37 -0400

    updated navigation to include user profile image and logout link

--- imports/ui/components/layout/Navigation.js
+++ imports/ui/components/layout/Navigation.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
+import { logout } from '../../../utils/auth'
+
 class Navigation extends Component {
     render() {
         return (
             <nav className='navigation'>
                 <ul>
-                    <li className='brand'>LOGO</li>
+                    <li className='profile'>
+                        <img src={Meteor.user().services.google.picture} alt='profile image' />
+                    </li>
                     <li>This Year</li>
                     <li>This Month</li>
                     <li>Today</li>
+                </ul>
+
+                <ul>
+                    <li onClick={logout}>Logout</li>
                 </ul>
             </nav>
         )
--- imports/ui/containers/App.js
+++ imports/ui/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import Auth from '../components/account/Auth'
 import Login from '../components/account/Login'
-import Logout from '../components/account/Logout'
 
 import Container from '../components/layout/Container'
 import Navigation from '../components/layout/Navigation'
@@ -14,8 +13,6 @@ class App extends Component {
             <Auth login={Login}>
                 {() => (
                     <Container>
-                        <Logout />
-                        <h3>hello {Meteor.user().services.google.email}</h3>
                         <Navigation />
                         <Page />
                     </Container>
