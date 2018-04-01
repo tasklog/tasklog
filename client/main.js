9a578dd49f7a07c8fccd0b3d3e630cf2553e26f4
nickbreaton <nick@breaton.com>
2018-03-31 20:42:31 -0400

    Fix login flashing

--- client/main.js
+++ client/main.js
@@ -3,7 +3,11 @@ import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 
 import App from '/imports/ui/containers/App'
+import { Tracker } from 'meteor/tracker'
 
 Meteor.startup(() => {
+    Tracker.autorun(() => {
+        console.log(Meteor.loggingIn())
+    })
     render(<App />, document.getElementById('render'))
 })
--- imports/ui/components/account/Auth.js
+++ imports/ui/components/account/Auth.js
@@ -2,37 +2,26 @@ import React, { Component } from 'react'
 import { Tracker } from 'meteor/tracker'
 
 class Auth extends Component {
-    static defaultProps = {
-        showLoginAfter: 250
-    }
-
     state = {
         loggedIn: false,
-        displayLogin: false
+        loggingIn: false
     }
-
     componentDidMount() {
         Tracker.autorun(() => {
-            const user = Meteor.user()
-            this.setState({ loggedIn: !!(user && user.services.google.email) })
+            this.setState({
+                loggedIn: !!Meteor.user(),
+                loggingIn: Meteor.loggingIn()
+            })
         })
-
-        // give small delay so login content does not flash
-        setTimeout(() => {
-            this.setState({ displayLogin: true })
-        }, this.props.showLoginAfter)
     }
-
     render() {
+        if (this.state.loggingIn) {
+            return null
+        }
         if (this.state.loggedIn) {
             return this.props.children()
         }
-
-        if (!this.state.loggedIn && this.state.displayLogin) {
-            return <this.props.login />
-        }
-        
-        return null
+        return <this.props.login />
     }
 }
 
