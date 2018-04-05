3a3831425d6487d617c02bc7f13d324b5a4538fa
nickbreaton <nick@breaton.com>
2018-04-05 14:51:57 -0400

    Secure google credentials

--- server/main.js
+++ server/main.js
@@ -1,16 +1,20 @@
 import { Meteor } from 'meteor/meteor'
+import dotenv from 'dotenv'
 
 import '/imports/api/tasks/methods'
 import '/imports/api/tasks/server/publications'
 import '/imports/api/users/server/publications'
 
 Meteor.startup(() => {
+    // read .env file for environment variables
+    dotenv.config()
+    // configure google if needed
     if (!ServiceConfiguration.configurations.findOne({ service: 'google' })) {
         ServiceConfiguration.configurations.insert({
             service: 'google',
-            clientId: '1026154799197-2fht9b4u943775lmaokpi8ijk8boio19.apps.googleusercontent.com',
+            clientId: '852673380073-qku8k0ed0dh407ini7tcq52hnq4mdq1o.apps.googleusercontent.com',
             loginStyle: 'popup',
-            secret: 'SDrISTVcyKLDQiZmHzyRae4C'
+            secret: process.env.GOOGLE_SECRET
         })
     }
 })
