f27b1811ae3d5e9ccc36658042ba65ab98afbba9
ethomp54 <ethomp54@uncc.edu>
2018-04-10 16:39:50 -0400

    added react router and routes for daily, weekly, montly, yearly logs

---
+++ imports/ui/components/log/DailyLog.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import Title from '/imports/ui/components/page/Title'
+
+import Tasks from '/imports/ui/components/account/Tasks'
+
+class DailyLog extends Component {
+    render() {
+        return (
+            <div>
+                <Title>March 31, 2018</Title>
+                <ul className='daily-log'>
+                    <Tasks>
+                        {tasks => tasks.map(task => (
+                            <li key={task._id} className='task'>
+                                {task.text}
+                            </li>
+                        ))}
+                    </Tasks>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default DailyLog
