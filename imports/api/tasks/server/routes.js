a39c56f0f256aaa6cb809c94f0cd6d749508aa09
nickbreaton <nick@breaton.com>
2018-04-14 20:55:39 -0400

    Add default API date

--- imports/api/tasks/server/routes.js
+++ imports/api/tasks/server/routes.js
@@ -1,9 +1,10 @@
 import { router } from '/server/router'
+import { createScheduledTimestamp } from '/imports/utils/time'
 
 router.post('/task/create', async (req, res) => {
     Meteor.users.find().fetch().forEach(user => {
         Meteor.runAsUser(user._id, () => {
-            Meteor.call('task.create', req.body.text)
+            Meteor.call('task.create', req.body.text, createScheduledTimestamp('day'))
         })
     })
     res.send('ok')
