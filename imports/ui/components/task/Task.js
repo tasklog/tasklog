a814157e80378875a56f50fa3b9d448254578515
ethomp54 <ethomp54@uncc.edu>
2018-04-16 10:28:07 -0400

    add functionality to change due date on task with react-day-picker

--- imports/ui/components/task/Task.js
+++ imports/ui/components/task/Task.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import { Meteor } from 'meteor/meteor'
 
+import DayPickerInput from 'react-day-picker/DayPickerInput'
+import 'react-day-picker/lib/style.css'
+import { formatDate, parseDate } from 'react-day-picker/moment'
+
 import Title from '/imports/ui/components/page/Title'
 
 class Task extends Component {
+    state = {
+        task: {},
+        selectedDay: undefined
+    }
+
+    componentDidMount() {
+        this.setState({
+            task: this.props.task,
+            selectedDay: this.props.task.due !== null ? this.props.task.due : undefined
+        })
+    }
+
     onComplete = (id) => {
         Meteor.call('task.toggle', id)
+    }
+
+    onChangeDue = (day) => {
+        Meteor.call('task.changeDue', this.state.task._id, day)
+        this.setState({ selectedDay: day })
     }
 
     onDelete = (id) => {
@@ -13,7 +34,7 @@ class Task extends Component {
     }
 
     render() {
-        const { task } = this.props
+        const { task, selectedDay } = this.state
 
         return (
             <li className='task'>
@@ -26,10 +47,23 @@ class Task extends Component {
                     />
                     <label htmlFor={`checkbox-${task._id}`}></label>
                 </div>
+
                 <span className={task.status === 'COMPLETE' ? 'completed' : ''}>
                     {task.text}
                 </span>
-                <span className='delete' aria-label='delete' onClick={() => this.onDelete(task._id)}>×</span>
+
+                <span className='right'>
+                    Due on &nbsp;
+                    <DayPickerInput
+                        formatDate={formatDate}
+                        parseDate={parseDate}
+                        format='LL'
+                        placeholder='Choose Date'
+                        value={selectedDay}
+                        onDayChange={this.onChangeDue}
+                    />
+                    <span className='delete' aria-label='delete' onClick={() => this.onDelete(task._id)} />
+                </span>
             </li>
         )
     }
