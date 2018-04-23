7f74dcde6486f1565664791c46687264907967c3
nickbreaton <nick@breaton.com>
2018-04-22 23:22:25 -0400

    Add login screen

--- imports/ui/components/account/Login.js
+++ imports/ui/components/account/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import GoogleButton from 'react-google-button'
 import { loginWithGoogle } from '/imports/utils/auth'
+import GoogleIcon from 'react-icons/lib/fa/google'
 
 const Login = () => (
-    <GoogleButton onClick={loginWithGoogle} />
+    <div className='login'>
+        <div className='login__content'>
+            <div className='login__content__logo' aria-hidden />
+            <button className='login__content__with-google' onClick={loginWithGoogle}>
+                <GoogleIcon /> Login with Google
+            </button>
+            <div aria-hidden className='login__content__or' />
+            <input type='email' placeholder='Email Address' className='login__content__input' />
+            <input type='password' placeholder='Password' className='login__content__input' />
+            <button className='login__content__login'>
+                Login
+            </button>
+        </div>
+    </div>
 )
 
 export default Login
