fea5226a07a7139b98c1f95f769195f2da2fba7e
nickbreaton <nick@breaton.com>
2018-04-23 22:45:15 -0400

    Switch params

--- imports/ui/components/layout/Graph/graph.js
+++ imports/ui/components/layout/Graph/graph.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3'
 
-export default function (svg, data, width, height) {
+export default function (data, svg, width, height) {
     const container = d3.select(svg)
 
     // EXAMPLE: writing text to DOM, you should probably do something similar
