d840e4162fffd84afb9bdad05798374e960014bd
nickbreaton <nick@breaton.com>
2018-04-24 09:53:50 -0400

    Add logo to login

--- imports/ui/components/account/Login.js
+++ imports/ui/components/account/Login.js
@@ -6,7 +6,7 @@ import GoogleIcon from 'react-icons/lib/fa/google'
 const Login = () => (
     <div className='login'>
         <div className='login__content'>
-            <div className='login__content__logo' aria-hidden />
+            <img src='/img/logo_dark.svg' alt='Tasklog Logo' className='login__content__logo' />
             <button className='login__content__with-google' onClick={loginWithGoogle}>
                 <GoogleIcon /> Login with Google
             </button>
